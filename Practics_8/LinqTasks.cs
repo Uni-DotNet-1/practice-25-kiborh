1db27aaf017d2ccf12bcb534e0a9ae876eecc0e9
kiborh <lisnychyi.oleksandr@chnu.edu.ua>
2026-05-22 21:43:05 +0300

    Implement missing return statements in LinqTasks methods

--- Practics_8/LinqTasks.cs
+++ Practics_8/LinqTasks.cs
@@ -16,53 +16,53 @@ namespace Practics_8
     {
         public IEnumerable<int> GetEvenNumbers(List<int> numbers)
         {
-            return ;
+            return numbers.Where(number => number % 2 == 0);
         }
 
         public int GetSum(List<int> numbers)
         {
-            return ;
+            return numbers.Sum();
         }
 
         public IEnumerable<string> GetLongStrings(List<string> strings)
         {
-            return ;
+            return strings.Where(text => text.Length > 3);
         }
 
 
         public Person FindPersonByName(List<Person> people, string name)
         {
-            return ;
+            return people.First(person => person.Name == name);
         }
 
         public IEnumerable<int> SortDescending(List<int> numbers)
         {
-            return ;
+            return numbers.OrderByDescending(number => number);
         }
 
         public IEnumerable<IGrouping<int, Person>> GroupByAge(List<Person> people)
         {
-            return ;
+            return people.GroupBy(person => person.Age);
         }
 
         public bool AnyNumberGreaterThanTen(List<int> numbers)
         {
-            return ;
+            return numbers.Any(number => number > 10);
         }
 
         public IEnumerable<string?> SelectNames(List<Person> people)
         {
-            return ;
+            return people.Select(person => person.Name);
         }
 
         public bool AllPositive(List<int> numbers)
         {
-            return ;
+            return numbers.All(number => number > 0);
         }
 
         public IEnumerable<int> ConcatenateLists(List<int> list1, List<int> list2)
         {
-            return ;
+            return list1.Concat(list2);
         }
 
 
